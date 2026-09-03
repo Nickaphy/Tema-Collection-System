cfc48fd893490d3e6b53ca9cb9f5460f165710e7
Kaaynx <grundet30@gmail.com>
2026-09-03 10:56:46 +0200

    User og aggregate root

---
+++ WatchWorld.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WatchWorld.Domain.ValueObjects;
+
+namespace WatchWorld.Domain.Entities
+{
+    public class User : Aggregateroot
+    {
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+        public string Address { get; private set; }
+        public string City { get; private set; }
+        public List<UserRating> Rating { get; private set; }
+    }
+}
