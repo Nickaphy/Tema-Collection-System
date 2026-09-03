76eb298e0a88980732649a3944dae2c4bdb2ab09
Kaaynx <grundet30@gmail.com>
2026-09-03 13:04:32 +0200

    User con

--- WatchWorld.Domain/Entities/User.cs
+++ WatchWorld.Domain/Entities/User.cs
@@ -20,5 +20,63 @@ namespace WatchWorld.Domain.Entities
 
 
         private User() { }
+
+        private User(
+            string firstName,
+            string lastName,
+            string phoneNumber,
+            string email,
+            string address,
+            string city,
+            string note,
+            string password,
+            bool isAdmin,
+            List<UserRating> rating)
+        {
+            Id = Guid.NewGuid();
+            if (string.IsNullOrWhiteSpace(firstName))
+                throw new UserInvalidInputException($"Du skal have et fornavn!");
+            FirstName = firstName;
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new UserInvalidInputException($"Du skal have et efternavn!");
+            LastName = lastName;
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+                throw new UserInvalidInputException($"Du skal udfylde dit telefonnummer!");
+            PhoneNumber = phoneNumber;
+            if (string.IsNullOrWhiteSpace(email))
+                throw new UserInvalidInputException($"Du skal udfylde din email!");
+            Email = email;
+            if (string.IsNullOrWhiteSpace(address))
+                throw new UserInvalidInputException($"Du skal udfylde din Adresse!");
+            Address = address;
+            if (string.IsNullOrWhiteSpace(city))
+                throw new UserInvalidInputException($"Du skal udfylde din by!");
+            City = city;
+            Note = note;
+            if (string.IsNullOrWhiteSpace(password))
+                throw new UserInvalidInputException($"Du skal oprette en adgangskode!");
+            Password = password;
+            IsAdmin = isAdmin;
+            List<UserRating> Rating = rating;
+        }
+
+        public static User Create(
+            string firstName,
+            string lastName,
+            string phoneNumber,
+            string email,
+            string address,
+            string city,
+            string note,
+            string password,
+            bool isAdmin,
+            List<UserRating> rating)
+        {
+            var user = new User(firstName, lastName, phoneNumber, email, address, city, note, password, isAdmin, rating);
+
+            return user;
+        }
+
+
     }
 }
