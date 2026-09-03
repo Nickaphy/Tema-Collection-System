d26629e2da1fba377fd2c3fc01775be6c6cc4017
JKTPFence <jktpyt@gmail.com>
2026-09-03 12:59:43 +0200

    Borrow fully functional

---
+++ WatchWorld.Domain/Enums/BorrowStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WatchWorld.Domain.Enums
+{
+    public enum BorrowStatus
+    {
+        Active,
+        Completed,
+        Cancelled
+    }
+}
