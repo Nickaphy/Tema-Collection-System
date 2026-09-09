553632ef30d3de9fbd70cfa6896c3987b57935c4
Nickpahy <nickdahchris@gmail.com>
2026-09-09 11:49:57 -0400

    Barebones: Docker automatic setup and api

--- WatchWorld.Application/Services/WatchesService.cs
+++ WatchWorld.Application/Services/WatchesService.cs
@@ -1,6 +1,7 @@
-﻿using WatchWorld.Application.Ports.InBound;
+using WatchWorld.Application.Ports.InBound;
 using WatchWorld.Application.Ports.OutBound;
-using WatchWorld.Domain;
+using WatchWorld.Domain.Entities;
+using WatchWorld.Domain.Enums;
 
 namespace WatchWorld.Application.Services;
 
@@ -11,5 +12,17 @@ public class WatchesService : IWatchesUseCase
     public WatchesService(IWatchesRepository repository)
     {
         _repository = repository;
+    }
+
+    public Task<Watches> CreateWatchAsync(string name, string modelNumber, int caseSize, CaseShapeEnum caseShapeEnum, CaseMaterialEnum caseMaterialEnum, MovementTypeEnum movementTypeEnum, string style, decimal originalPrice, GenderEnum genderEnum, DateOnly releaseYear, List<BraceletTypeEnum> braceletTypeEnum, string description, List<HighResImage> images, CancellationToken cancellationToken = default)
+    {
+        // TODO: IWatchesRepository has no save/add method yet, so this isn't persisted anywhere.
+        var watch = Watches.Create(name, modelNumber, caseSize, caseShapeEnum, caseMaterialEnum, movementTypeEnum, style, originalPrice, genderEnum, releaseYear, braceletTypeEnum, description, images);
+        return Task.FromResult(watch);
+    }
+
+    public async Task<IEnumerable<Watches?>> GetAllAsync(CancellationToken ct = default)
+    {
+        return await _repository.GetAllAsync(ct);
     }
 }
