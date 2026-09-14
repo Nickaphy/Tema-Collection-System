b914c83c8822b70ea90432e16df074640fe2b94d
SJT92 <thorgaard@live.dk>
2026-09-14 11:08:10 +0200

    Piszh

--- WatchWorld.Application/Commands/UserRatingCommands/CreateUserRatingCommand.cs
+++ WatchWorld.Application/Commands/UserRatingCommands/CreateUserRatingCommand.cs
@@ -4,10 +4,10 @@ using WatchWorld.Domain.Enums;
 namespace WatchWorld.Application.Commands.UserRatingCommands
 {
     public record CreateUserRatingCommand(
+        Guid ratedByUserId,
         Guid ratedToUserId,
         int ratingAmount,
-        string description,
-        Guid ratedByUserId
+        string description
     )
     {
     }
